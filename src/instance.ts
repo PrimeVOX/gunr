38acc974405238a23a222b21686382027093752d
Aaron <aaron@primevox.net>
2020-08-27 17:48:30 -0500

    update type

--- src/instance.ts
+++ src/instance.ts
@@ -1,7 +1,7 @@
 import gunr from './mailgun';
 import { IMailgunConfig, Gunr } from './types';
 
-let instance: Gunr<'signup' | 'reset' | 'newsletter' | 'invoice' | 'receipt' | 'chargefail'>;
+let instance: Gunr<'signup' | 'reset' | 'newsletter' | 'invoice' | 'receipt' | 'chargefail' | 'overdue' | 'disconnect1' | 'disconnect2' | 'disconnect3'>;
 
 export function getInstance(apiKey?: string | IMailgunConfig, apiDomain?: string) {
   if (!instance)
